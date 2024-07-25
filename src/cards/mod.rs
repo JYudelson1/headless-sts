814cd8d31a5d892506f9b77d622eea7893e27f11
Joey <jyudel@gmail.com>
2024-07-25 15:16:38 -0400

    unused deps

--- src/cards/mod.rs
+++ src/cards/mod.rs
@@ -6,14 +6,8 @@ mod starter_decks;
 
 pub use card::CardName;
 pub use card_actions::CardActions;
-use card_trait::Card;
 pub use card_trait::MasterCard;
 pub use starter_decks::make_starter_deck;
 
 #[derive(PartialEq, Eq)]
 pub struct CardIndex(pub u8);
-
-pub struct CardInHand {
-    pub card: Box<dyn Card>,
-    pub id: uuid::Uuid,
-}
