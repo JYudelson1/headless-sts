5af525e7c0d863a7667b425b536ffd4740b45949
Joey <jyudel@gmail.com>
2024-08-07 17:14:18 -0400

    Adding todo

--- src/combat/setup_combat.rs
+++ src/combat/setup_combat.rs
@@ -2,6 +2,7 @@ use crate::{cardrewardrng::CombatType, enemies::EnemyType, utils::Act};
 
 pub fn get_enemies(act: &Act, floor: u8, combat_type: CombatType) -> Vec<EnemyType> {
     // TODO: Is there something weird about elite enemies? Like they dont happen twice?
+    // TODO: Easy pools
     vec![EnemyType::JawWorm]
     //todo!()
 }
